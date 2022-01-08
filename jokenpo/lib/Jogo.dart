59c43f80bb29b83d05bbbd52a0987a70cd5cc5dd
Wilson Junior <wilsonraj@unipam.edu.br>
2022-01-08 17:49:34 -0300

    Criação do projeto e da classe Jogo

---
+++ jokenpo/lib/Jogo.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  const Jogo({ Key? key }) : super(key: key);
+
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo") ,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center ,
+        children: <Widget>[
+          //text
+          //imagem
+          //text resultado
+          //linha 3 imagens
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16) ,
+            child: Text(
+              "Escolha do App" ,
+              textAlign: TextAlign.center ,
+              style: TextStyle(
+                fontSize: 20 ,
+                fontWeight: FontWeight.bold
+              ),
+            )
+          ),
+          Image.asset("images/padrao.png") ,
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16) ,
+            child: Text(
+              "Escolha uma opção abaixo" ,
+              textAlign: TextAlign.center ,
+              style: TextStyle(
+                fontSize: 20 ,
+                fontWeight: FontWeight.bold
+              ),
+            )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+            children: <Widget>[
+              Image.asset("images/pedra.png", height: 100,) ,
+              Image.asset("images/papel.png", height: 100,) ,
+              Image.asset("images/tesoura.png", height: 100,)
+            ]
+          )
+        ]
+      )
+    );
+  }
+}
